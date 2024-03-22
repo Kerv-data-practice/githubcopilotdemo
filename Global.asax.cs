c695b286c4a611bd40614762f2702eeefd2d3fa3
edyaukerv <136830770+edyaukerv@users.noreply.github.com>
2024-03-22 22:29:34 +0000

    Add files via upload

---
+++ Global.asax.cs
@@ -0,0 +1,181 @@
+﻿
+#define DEBUG
+using System;
+using System.Configuration;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Mvc;
+using Autofac;
+using Autofac.Integration.WebApi;
+using Azure.Identity;
+using Azure.Messaging.ServiceBus.Administration;
+using Azure.Security.KeyVault.Secrets;
+using GetD365DataAPI.Controllers;
+using GetD365DataAPI.models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureAppConfiguration;
+using Microsoft.Xrm.Sdk;
+using Newtonsoft.Json;
+using ConfigurationBuilder = Microsoft.Extensions.Configuration.ConfigurationBuilder;
+
+namespace GetD365DataAPI
+{
+    public class WebApiApplication : System.Web.HttpApplication, IDisposable
+    {
+        public static IConfiguration Configuration;
+        private static IConfigurationRefresher _configurationRefresher;
+        private IContainer _container;
+
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+
+            // Initialize configuration and services
+            SetupConfiguration();
+            RegisterServices();
+            // Create an instance of the BackgroundService and start it
+            var backgroundService = new BackgroundService(Configuration);
+            backgroundService.Start();
+        }
+
+        private void SetupConfiguration()
+        {
+            Configuration = new ConfigurationBuilder()
+                .AddAzureAppConfiguration(options =>
+                {
+                    options.Connect(System.Configuration.ConfigurationManager.AppSettings["AzureAppConfigConnectionString"])
+                        .Select("WebAPI:Entities:*")
+                        .ConfigureRefresh(refresh =>
+                        {
+                            refresh.Register("WebAPI:Settings:Sentinel", refreshAll: true)
+                                .SetCacheExpiration(new TimeSpan(0, 0, 10));
+                        });
+                    _configurationRefresher = options.GetRefresher();
+                })
+                .Build();
+
+            // Create a SecretClient using the specified managed identity
+            var keyVaultUri = new Uri(System.Configuration.ConfigurationManager.AppSettings["KeyVaultUri"]);
+            var secretClient = new SecretClient(keyVaultUri, new DefaultAzureCredential());
+
+            // Retrieve all secrets from Key Vault
+            var allSecrets = secretClient.GetPropertiesOfSecrets()
+                .Select(secretItem => secretClient.GetSecret(secretItem.Name).Value)
+                .ToList();
+
+            // Load all secrets into configuration
+            foreach (var secret in allSecrets)
+            {
+                Configuration["KV:" + secret.Name] = secret.Value;
+            }
+
+
+            //initilise service bus 
+
+            //            var webApiKeys = Configuration.AsEnumerable()
+            //                .Where(kvp => kvp.Key.StartsWith("WebAPI:Entities:"))
+            //                .ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
+
+            //            foreach (var kvp in webApiKeys)
+            //            {
+            //                string keyIdentifier = kvp.Key.Split(':')[2];
+            //                string value = kvp.Value;
+            //                ConfigEntity entityConfig = JsonConvert.DeserializeObject<ConfigEntity>(value);
+            //#if !DEBUG
+            //                SetupServiceBusTopic(entityConfig, keyIdentifier);
+            //#endif
+            //            }
+            //TODO:Fix service bus initilisation
+#if !DEBUG
+                SetupServiceBusTopic(entityConfig, keyIdentifier);
+#endif
+
+        }
+        private void SetupServiceBusTopic(ConfigEntity entityConfig, string keyIdentifier)
+        {
+            string serviceBusNamespace = System.Configuration.ConfigurationManager.AppSettings["AzureServiceBusNamespace"];
+            string fullyQualifiedNamespace = $"{serviceBusNamespace}.servicebus.windows.net";
+            
+            // Create a ServiceBusAdministrationClient using DefaultAzureCredential
+            ServiceBusAdministrationClient managementClient = new ServiceBusAdministrationClient(fullyQualifiedNamespace, new DefaultAzureCredential());
+
+            string topicName = entityConfig.EntityName;
+
+            // Check if the topic with the same name exists, if not, create it
+            if (!managementClient.TopicExistsAsync(topicName).Result)
+            {
+                managementClient.CreateTopicAsync(topicName).Wait();
+            }
+
+            // Create subscriptions based on the connector
+            foreach (var output in entityConfig.output)
+            {
+                string subscriptionName = output.connector;
+
+                // Check if the subscription with the same name exists, if not, create it
+                if (!managementClient.SubscriptionExistsAsync(topicName, subscriptionName).Result)
+                {
+                   // managementClient.CreateSubscriptionAsync(topicName, subscriptionName).Wait();
+                    managementClient.CreateSubscriptionAsync(
+                        new CreateSubscriptionOptions(topicName, subscriptionName),
+                        new CreateRuleOptions("DefaultFilter", new SqlRuleFilter($"user.KI='{keyIdentifier}'"))).Wait();
+
+                }
+                
+                
+            }
+        }
+
+
+
+
+        private void RegisterServices()
+        {
+            var builder = new ContainerBuilder();
+
+            // Initialize the ConnectionHelper with the configuration
+            ConnectionHelper.Initialize(Configuration);
+
+            // Register your services and dependencies here
+            builder.Register(c => ConnectionHelper.CreateOrganizationService()).As<IOrganizationService>();
+
+            // Register IConfigurationRefresher as an instance
+            builder.RegisterInstance(_configurationRefresher).As<IConfigurationRefresher>();
+
+            // Register IConfiguration as an instance (assuming Configuration is your instance of IConfiguration)
+            builder.RegisterInstance(Configuration).As<IConfiguration>();
+
+            // Register the Web API controllers
+            builder.RegisterApiControllers(typeof(WebApiApplication).Assembly);
+
+            // Build the Autofac container
+            _container = builder.Build();
+
+            // Set the Web API dependency resolver to use Autofac
+            GlobalConfiguration.Configuration.DependencyResolver = new AutofacWebApiDependencyResolver(_container);
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            try
+            {
+                // Refresh configuration asynchronously
+                _ = _configurationRefresher.TryRefreshAsync().GetAwaiter().GetResult();
+            }
+            catch (Exception ex)
+            {
+                // Handle or log the exception appropriately
+                // Consider using a dedicated error handling mechanism
+                
+            }
+        }
+
+        public void Dispose()
+        {
+            // Dispose of Configuration and ConfigurationRefresher if needed
+            // For example, ConfigurationRefresher.Dispose() if it implements IDisposable
+        }
+    }
+}
---
+++ Scheduler.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Configuration;
+using System.Net.Http;
+using System.Timers;
+using GetD365DataAPI.models;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+
+namespace GetD365DataAPI
+{
+    public class BackgroundService
+    {
+        private readonly IConfiguration _configuration;
+       // private  HttpClient _httpClient;
+        private readonly Timer _timer;
+        //private readonly ILogger _logger;
+
+        public BackgroundService(IConfiguration configuration )
+        {
+            _configuration = configuration;
+            _timer = new Timer(30000); // Set the timer interval to 1 minute (adjust as needed)
+            _timer.Elapsed += (sender, e) => ExecuteScheduledCalls();
+        }
+
+        public void Start()
+        {
+            _timer.Start();
+        }
+
+        private void ExecuteScheduledCalls()
+        {
+            foreach (var key in _configuration.AsEnumerable())
+            {
+                if (key.Key.StartsWith("WebAPI:Entities:"))
+                {
+                    string routeName =  "/api/" + key.Key.Split(':')[2];
+                    var jsonObject = JsonConvert.DeserializeObject<ConfigEntity>(key.Value);
+                    string frequency = jsonObject.RefreshInterval;
+
+                    string[] frequencyparts = frequency.Split(',');
+                    
+                    // Parse the frequency setting to calculate milliseconds
+                    if (frequencyparts.Length == 3)
+                    {
+                        TimeSpan interval = new TimeSpan(int.Parse(frequencyparts[0]), int.Parse(frequencyparts[1]),
+                            int.Parse(frequencyparts[2]));
+                        var timer = new Timer(interval.TotalMilliseconds);
+                        //TODO: review if this needs to be run at first run
+                        timer.Elapsed += (sender, e) => ExecuteApiCall(routeName);
+                        timer.Start();
+
+                        Console.WriteLine(
+                            $"Scheduled API call for route: {routeName}, endpoint: {routeName}, frequency: {frequency}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Invalid frequency format for route: {routeName}");
+                    }
+                }
+            }
+        }
+
+        private void ExecuteApiCall(string endpoint)
+        {
+            try
+            {
+                var baseAddress = new Uri(System.Configuration.ConfigurationManager.AppSettings["BaseURL"]);
+                using (var client = new HttpClient() { BaseAddress = baseAddress })
+
+                {
+                    // Make an HTTP GET request using the relative URI (endpoint)
+                    //TODO: Review the BASE URL Settings
+
+                    HttpResponseMessage response = client.GetAsync(  endpoint).Result;
+                    //TODO: avoid duplicate calls to the same API
+                    if (response.IsSuccessStatusCode)
+                    {
+                        // API call was successful
+                        Console.WriteLine($"API call to {endpoint} succeeded at {DateTime.Now}");
+                    }
+                    else
+                    {
+                        // API call failed
+                        Console.WriteLine($"API call to {endpoint} failed. Status Code: {response.StatusCode}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle exceptions that may occur during the API call
+                Console.WriteLine($"API call to {endpoint} failed with an exception: {ex.Message}");
+            }
+        }
+    }
+
+}
